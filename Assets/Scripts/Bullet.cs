42145ebdc592d2d287cede1d532116809b1c288c
GeorgeDenis <george.duluta@yahoo.com>
2024-03-05 15:05:19 +0200

    bullet collide effect

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Transform target;
+    public GameObject impactEffect;
+
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = 15f * Time.deltaTime;
+        if (dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+    }
+    void HitTarget()
+    {
+        GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effectIns, 2f);
+        Destroy(gameObject);
+    }
+}
