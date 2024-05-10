33b1958d9b0d6721d127978df18045923eb46569
GeorgeDenis <george.duluta@yahoo.com>
2024-05-10 18:03:04 +0300

    upgrade and sell functionalities

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -24,20 +24,7 @@ public class BuildManager : MonoBehaviour
     public bool CanBuild { get { return turretToBuild != null; } }
 
     public bool HasMoney { get { return PlayerStats.Money >= turretToBuild.cost; } }
-    public void BuildTurretOn(Node node)
-    {
-        if (PlayerStats.Money < turretToBuild.cost)
-        {
-            Debug.Log("Not enough money to build that!");
-            return;
-        }
-        PlayerStats.Money -= turretToBuild.cost;
-        GameObject turret =(GameObject)Instantiate(turretToBuild.prefab,node.GetBuildPosition(),Quaternion.identity);
-        node.turret = turret;
-        GameObject effect = (GameObject)Instantiate(buildEffect, node.GetBuildPosition(), Quaternion.identity);
-        Destroy(effect,5f);
-        Debug.Log("Turret build! Money left: " + PlayerStats.Money);
-    }
+
     public void SelectNode(Node node)
     {
         if (selectedNode == node)
@@ -60,5 +47,9 @@ public class BuildManager : MonoBehaviour
         turretToBuild = turret;
         DeselectNode();
     }   
+    public TurretBlueprint GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
 
 }
